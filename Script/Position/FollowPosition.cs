43560f8a9f9e4a49ec42b5913b41f8a05a595460
Chu Wing Kin <cwksc.person@gmail.com>
2019-08-30 22:58:42 +0800

    Update Position Script (Follow Position and Lock Position)

---
+++ Script/Position/FollowPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour
+{
+
+    public GameObject target;
+
+    Vector3 offset;
+    Vector3 oldPosition;
+
+    void Start()
+    {
+        oldPosition = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = target.transform.position - oldPosition;
+        transform.position += offset;
+        oldPosition = target.transform.position;
+    }
+}
