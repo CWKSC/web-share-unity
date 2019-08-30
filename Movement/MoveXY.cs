5890ff4f00c2cdee94ddab2ec87f7d9f7554c093
Chu Wing Kin <cwksc.person@gmail.com>
2019-08-30 22:57:03 +0800

    Update Movement Script

---
+++ Movement/MoveXY.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveXY : MonoBehaviour
+{
+
+    public float xSpeed = 1;
+    public float ySpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed,
+                                         transform.position.y + Input.GetAxis("Vertical")   * Time.deltaTime * ySpeed,
+                                         transform.position.z);
+    }
+}
---
+++ Movement/MoveXZ.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveXZ : MonoBehaviour
+{
+    public float xSpeed = 1;
+    public float zSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed,
+                                         transform.position.y,
+                                         transform.position.z + Input.GetAxis("Vertical") * Time.deltaTime * zSpeed);
+    }
+}
