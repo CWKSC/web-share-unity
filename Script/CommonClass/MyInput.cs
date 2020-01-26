728daefaee7c586eecca1dbe3f0f24b28428967c
CWKSC <cwksc.person@gmail.com>
2020-01-26 19:48:18 +0800

    Upload Script

---
+++ Script/CommonClass/MyInput.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyInput
+{
+
+    #region Left Mouse 
+    public static void LeftMouse(Action action)
+    {
+        if (Input.GetMouseButton(0)) { action(); }
+    }
+    public static void LeftMouseDown(Action action)
+    {
+        if (Input.GetMouseButtonDown(0)) { action(); }
+    }
+    public static void LeftMouseUp(Action action)
+    {
+        if (Input.GetMouseButtonUp(0)) { action(); }
+    }
+    #endregion
+
+    #region Right Mouse
+    public static void RightMouse(Action action)
+    {
+        if (Input.GetMouseButton(1)) { action(); }
+    }
+    public static void RightMouseDown(Action action)
+    {
+        if (Input.GetMouseButtonDown(1)) { action(); }
+    }
+    public static void RightMouseUp(Action action)
+    {
+        if (Input.GetMouseButtonUp(1)) { action(); }
+    }
+    #endregion
+
+    #region Middle Mouse
+    public static void MiddleMouse(Action action)
+    {
+        if (Input.GetMouseButton(2)) { action(); }
+    }
+    public static void MiddleMouseDown(Action action)
+    {
+        if (Input.GetMouseButtonDown(2)) { action(); }
+    }
+    public static void MiddleMouseUp(Action action)
+    {
+        if (Input.GetMouseButtonUp(2)) { action(); }
+    }
+    #endregion
+
+}
--- Script/CommonClass/PositionSystem.cs
+++ Script/CommonClass/PositionSystem.cs
@@ -7,14 +7,45 @@ public class PositionSystem : MonoBehaviour
 
     public static Vector3 mousePositionOffset = new Vector3(Screen.width / 2, Screen.height / 2);
 
-    public static Vector3 getUIElementPositionAtMouse()
+    public static Vector3 GetUIElementPositionAtMouse()
     {
         return Input.mousePosition - mousePositionOffset;
     }
-    public static void setUIElementPositionAtMouse(Transform UIelement)
+    public static void SetUIElementPositionAtMouse(Transform UIelement)
     {
         UIelement.localPosition = Input.mousePosition - mousePositionOffset;
     }
 
+
+    public static List<Vector3> CreateHexPositionList(Vector3 originPosition, Vector3 forwardVector, Vector3 rightVector, int level, float distanceBetweenHexPosition = 0.18f)
+    {
+        List<Vector3> hexPositionList = new List<Vector3>{ originPosition };
+        for (int i = 1; i <= level; i++)
+        {
+            Vector3 hexPosition0 = originPosition + (Quaternion.AngleAxis(90, forwardVector) * rightVector).normalized * distanceBetweenHexPosition * i;
+            Vector3 hexPosition1 = originPosition + (Quaternion.AngleAxis(90 + 60, forwardVector) * rightVector).normalized * distanceBetweenHexPosition * i;
+            Vector3 hexPosition2 = originPosition + (Quaternion.AngleAxis(90 + 60 * 2, forwardVector) * rightVector).normalized * distanceBetweenHexPosition * i;
+            Vector3 hexPosition3 = originPosition + (Quaternion.AngleAxis(90 + 60 * 3, forwardVector) * rightVector).normalized * distanceBetweenHexPosition * i;
+            Vector3 hexPosition4 = originPosition + (Quaternion.AngleAxis(90 + 60 * 4, forwardVector) * rightVector).normalized * distanceBetweenHexPosition * i;
+            Vector3 hexPosition5 = originPosition + (Quaternion.AngleAxis(90 + 60 * 5, forwardVector) * rightVector).normalized * distanceBetweenHexPosition * i;
+            hexPositionList.Add(hexPosition0);
+            hexPositionList.Add(hexPosition1);
+            hexPositionList.Add(hexPosition2);
+            hexPositionList.Add(hexPosition3);
+            hexPositionList.Add(hexPosition4);
+            hexPositionList.Add(hexPosition5);
+            for (int j = 1; j < i; j++)
+            {
+                float rate = (float)j / i;
+                hexPositionList.Add(Vector3.Lerp(hexPosition0, hexPosition1, rate));
+                hexPositionList.Add(Vector3.Lerp(hexPosition1, hexPosition2, rate));
+                hexPositionList.Add(Vector3.Lerp(hexPosition2, hexPosition3, rate));
+                hexPositionList.Add(Vector3.Lerp(hexPosition3, hexPosition4, rate));
+                hexPositionList.Add(Vector3.Lerp(hexPosition4, hexPosition5, rate));
+                hexPositionList.Add(Vector3.Lerp(hexPosition5, hexPosition0, rate));
+            }
+        }
+        return hexPositionList;
+    }
 
 }
